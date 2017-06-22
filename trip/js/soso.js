106c4c1ff0a5d0c2740da10ab22bb286eac84ad9
chenquanyu7 <915894676@qq.com>
2017-06-22 11:18:23 +0800

    变3

--- trip/js/soso.js
+++ trip/js/soso.js
@@ -18,11 +18,13 @@ window.onload = function() {
 		$(".back_one").on("tap",function(){
 			$("#box").css("display","block");
 			$("#twobox").css("display","none");
+			$(".lists").html("");
+			$(".loding").css("display","block");
 		});
 		
 //		 $(".lists").html("");
 				$(".but").on("tap",function(){
-			
+			 $(".lists").html("");
 				$("#box").css("display","none");
 			$("#twobox").css("display","block");
 		
@@ -34,7 +36,14 @@ window.onload = function() {
 //$.get("http://proxy.e12e.com/?http://www.webxml.com.cn/WebServices/TrainTimeWebService.asmx/getStationAndTimeByStationName?StartStation=长沙&ArriveStation=广州&UserID=75f58aa83d8d47efb16c41040d7e7764",
 //	   function(data){
 	if(inp!="" && ipo!=""){
-		$.get("http://proxy.e12e.com/?http://ws.webxml.com.cn/WebServices/TrainTimeWebService.asmx/getStationAndTimeByStationName?UserID=86e1ae702ed5465d9b59be1f1f64cce4",
+		if($("#txtone").hasClass("tr")){
+			
+					v=$("#txtone").val();
+					$("#txtone").val($("#ttxtwo").val());
+					$("#ttxtwo").val(v);
+					 inp=$("#txtone").val();
+		              ipo=$("#ttxtwo").val();
+					$.get("http://proxy.e12e.com/?http://ws.webxml.com.cn/WebServices/TrainTimeWebService.asmx/getStationAndTimeByStationName?UserID=86e1ae702ed5465d9b59be1f1f64cce4",
 	   {StartStation:inp,ArriveStation:ipo},function(data){
 	   	console.log($(data));
 	   	console.log(data);
@@ -59,40 +68,52 @@ window.onload = function() {
 	   	})
 
         $(".lists").html(str);
-//      <li>
-//					<div class="t_box">
-//						<div>
-//							<p>海口</p>
-//							<p>11:14</p>
-//						</div>
-//						<div>
-//							<p>Z386</p>
-//						    <p>11时12分</p>
-//						</div>
-//						<div>
-//							<p>广州</p>
-//							<p>
-//                    22:26
-//							</p>
-//						</div>
-//						<div><em>¥</em>
-//							<strong>139</strong>
-//							起
-//						</div>
-//					</div>
-//					<div class="b_box">
-//						<a href="">硬座:0张<span>(抢)</span></a>
-//						<a href="">硬卧:99张</a>
-//						<a href="">软卧:4张</a>
-//						
-//						
-//						
-//					</div>
-//				</li>
-//      
-//   
+$(".loding").css("display","none");
+     
 	   })
+		}
+		else{
+					$.get("http://proxy.e12e.com/?http://ws.webxml.com.cn/WebServices/TrainTimeWebService.asmx/getStationAndTimeByStationName?UserID=86e1ae702ed5465d9b59be1f1f64cce4",
+	   {StartStation:inp,ArriveStation:ipo},function(data){
+	   	console.log($(data));
+	   	console.log(data);
+	   	var str="";
+	 console.log($(data).find("FirstStation").text());
+     	$(data).find("TimeTable").each(function(index,obj){
+     		  	
+	   	var stime=$(obj).find("StartTime").text();//开始时间
+	   	var bc=$(obj).find("TrainCode").text();//班次
+	   	var ustime=$(obj).find("UseDate").text();//耗时
+	   	var daoda=$(obj).find("ArriveTime").text();//到达时间
+	   	var zdian=$(obj).find("LastStation").text();//终点站
+	   	var qdian=$(obj).find("FirstStation").text();//起点站
+	   		if(index>10) return;	
+//	   	str+="<li><h3><span class='bco'>"+bc+"</span>&nbsp;<span>"+(qdian+"-"+zdian)+"<span><h3>";
+//	   	str+="<p><span>出发时间:"+stime+"</span><br/>到达时间<span>"+daoda+"</span>"+"</p>";
+//	   	str+="<p>总用时:"+ustime+"</p></li>";
+        str+="<li><div class='t_box'><div><p>"+qdian+"</p><p>"+stime+"</p></div><div><p>"+bc+"</p>";
+        str+="<p>"+ustime+"</p></div><div><p>"+zdian+"</p><p>"+daoda+"</p></div><div><em>¥</em><strong>139</strong>起";
+        str+="</div></div><div class='b_box'><a href='#'>硬座:0张<span>(抢)</span></a><a href='#'>硬卧:99张</a>";
+        str+="<a href='#'>软卧:4张</a></div></li>";
+	   	})
+
+        $(".lists").html(str);
+$(".loding").css("display","none");
+	   })
+		}
+		 
+
 	}
+	
+	else{
+			var str="";
+			alert($(".lists li").length);
+			setTimeout(function(){
+				str+="<li class='lit'><img src='img/1.png'/><p class='ps'>抱歉，没有相关信息!</p></li>";
+			 $(".lists").html(str);
+			 $(".loding").css("display","none");
+			},3000);
+       }
 	
 	})
 	})
